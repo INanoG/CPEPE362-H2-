75534cb57a48d7fde78e50114d388ab2bc86f555
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-04-26 15:50:39 +0800

    tried to start coding for login page [Can be changed for planned layout]

---
+++ bookedwebapp/booked_webapp_v1/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_2.10.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Computer\\Desktop\\Gogo Files\\3rd Year\\2nd Sem\\C2\\CPEPE362\\CPEPE362-H2\\bookedwebapp\\booked_webapp_v1" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_2.10.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Computer\\Desktop\\Gogo Files\\3rd Year\\2nd Sem\\C2\\CPEPE362\\CPEPE362-H2\\bookedwebapp\\booked_webapp_v1"
+  "FLUTTER_ROOT=C:\\flutter_windows_2.10.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Computer\\Desktop\\Gogo Files\\3rd Year\\2nd Sem\\C2\\CPEPE362\\CPEPE362-H2\\bookedwebapp\\booked_webapp_v1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Computer\\Desktop\\Gogo Files\\3rd Year\\2nd Sem\\C2\\CPEPE362\\CPEPE362-H2\\bookedwebapp\\booked_webapp_v1"
+  "FLUTTER_TARGET=C:\\Users\\Computer\\Desktop\\Gogo Files\\3rd Year\\2nd Sem\\C2\\CPEPE362\\CPEPE362-H2\\bookedwebapp\\booked_webapp_v1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Computer\\Desktop\\Gogo Files\\3rd Year\\2nd Sem\\C2\\CPEPE362\\CPEPE362-H2\\bookedwebapp\\booked_webapp_v1\\.dart_tool\\package_config.json"
+)
