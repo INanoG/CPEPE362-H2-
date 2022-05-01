047255497935a668ca4b8d6da69f62b60a75c704
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-05-01 18:24:22 +0800

    a bit of changes

--- bookedwebapp/booked_webapp_v1/lib/views/layout_template/layout_template.dart
+++ bookedwebapp/booked_webapp_v1/lib/views/layout_template/layout_template.dart
@@ -29,7 +29,7 @@ class LayoutTemplate extends StatelessWidget {
                 child: Navigator(
                   key: locator<NavigationService>().navigatorKey,
                   onGenerateRoute: generateRoute,
-                  initialRoute: HomeRoute,
+                  initialRoute: WelcomeRoute,
                 ),
               ),
             ],
