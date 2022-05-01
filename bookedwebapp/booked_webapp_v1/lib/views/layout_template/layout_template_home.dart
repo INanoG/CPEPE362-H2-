5ed73b76b0a827d3327dfe8b037d02be79500fe8
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-05-01 18:51:13 +0800

    added another layout template for home page after sign in

---
+++ bookedwebapp/booked_webapp_v1/lib/views/layout_template/layout_template_home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:booked_webapp_v1/locator.dart';
+import 'package:booked_webapp_v1/routing/route_names.dart';
+import 'package:booked_webapp_v1/routing/router.dart';
+import 'package:booked_webapp_v1/services/navigation_service.dart';
+import 'package:booked_webapp_v1/views/welcome_page/welcome_page_desktop.dart';
+import 'package:booked_webapp_v1/views/welcome_page/welcome_page_mobile.dart';
+import 'package:booked_webapp_v1/widgets/centered_view/centered_view.dart';
+import 'package:booked_webapp_v1/widgets/navigation_bar/navigation_bar.dart';
+import 'package:booked_webapp_v1/widgets/navigation_drawer/navigation_drawer.dart';
+
+class LayoutTemplateHome extends StatelessWidget {
+  const LayoutTemplateHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+        drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
+            ? NavigationDrawer()
+            : null,
+        backgroundColor: Colors.white,
+        body: Centered_View(
+          child: Column(
+            children: <Widget>[
+              const Navigation_Bar(),
+              Expanded(
+                child: Navigator(
+                  key: locator<NavigationService>().navigatorKey,
+                  onGenerateRoute: generateRoute,
+                  initialRoute: WelcomeRoute,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
