f1b505b80894c800be0d77095169346b9c10913e
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-05-01 23:35:12 +0800

    forgot to add functionality for get started button from mobile screen part

--- bookedwebapp/booked_webapp_v1/lib/widgets/call_to_action/call_to_action_mobile.dart
+++ bookedwebapp/booked_webapp_v1/lib/widgets/call_to_action/call_to_action_mobile.dart
@@ -1,3 +1,6 @@
+import 'package:booked_webapp_v1/locator.dart';
+import 'package:booked_webapp_v1/routing/route_names.dart';
+import 'package:booked_webapp_v1/services/navigation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:booked_webapp_v1/constants/app_colors.dart';
 
@@ -7,24 +10,31 @@ class CallToActionMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      alignment: Alignment.center,
-      child: Text(
-        title,
-        style: const TextStyle(
-            fontSize: 18, fontWeight: FontWeight.w800, color: Colors.white),
-      ),
-      decoration: BoxDecoration(
-        color: primaryColor,
-        borderRadius: BorderRadius.circular(5),
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromARGB(255, 55, 83, 56),
-            blurRadius: 1,
-            offset: Offset(0, 3),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () {
+        // dont ever use a service directly in the ui to change any kind of state
+        // services should only be used from a viewmodel
+        locator<NavigationService>().navigateTo(LoginRoute);
+      },
+      child: Container(
+        height: 60,
+        alignment: Alignment.center,
+        child: Text(
+          title,
+          style: const TextStyle(
+              fontSize: 18, fontWeight: FontWeight.w800, color: Colors.white),
+        ),
+        decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.circular(5),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(255, 55, 83, 56),
+              blurRadius: 1,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
       ),
     );
   }
