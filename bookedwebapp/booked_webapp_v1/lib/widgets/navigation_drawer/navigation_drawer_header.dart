8f29039f00a8dcd6cba4e3d08554b0cffef23037
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-05-01 19:37:00 +0800

    panel on the navigation drawer now working

--- bookedwebapp/booked_webapp_v1/lib/widgets/navigation_drawer/navigation_drawer_header.dart
+++ bookedwebapp/booked_webapp_v1/lib/widgets/navigation_drawer/navigation_drawer_header.dart
@@ -1,3 +1,6 @@
+import 'package:booked_webapp_v1/locator.dart';
+import 'package:booked_webapp_v1/routing/route_names.dart';
+import 'package:booked_webapp_v1/services/navigation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:booked_webapp_v1/constants/app_colors.dart';
 
@@ -6,26 +9,33 @@ class NavigationDrawerHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      color: primaryColor,
-      alignment: Alignment.center,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: const <Widget>[
-          Text(
-            'TALK WITH BOOKWORMS',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w800,
-              color: Colors.white,
+    return GestureDetector(
+      onTap: () {
+        // dont ever use a service directly in the ui to change any kind of state
+        // services should only be used from a viewmodel
+        locator<NavigationService>().navigateTo(RegisterRoute);
+      },
+      child: Container(
+        height: 150,
+        color: primaryColor,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const <Widget>[
+            Text(
+              'TALK WITH BOOKWORMS',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+              ),
             ),
-          ),
-          Text(
-            'TAP HERE',
-            style: TextStyle(color: Colors.white),
-          )
-        ],
+            Text(
+              'TAP TO JOIN',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
       ),
     );
   }
