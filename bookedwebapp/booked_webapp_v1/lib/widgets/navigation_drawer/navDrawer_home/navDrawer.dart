c377b4e26a197d5dc628ca56f92821a00cc1b903
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-05-01 20:13:38 +0800

    added files for nav drawer in home page

---
+++ bookedwebapp/booked_webapp_v1/lib/widgets/navigation_drawer/navDrawer_home/navDrawer.dart
@@ -0,0 +1,28 @@
+import 'package:booked_webapp_v1/widgets/navigation_drawer/navDrawer_home/navDrawer_header_home.dart';
+import 'package:flutter/material.dart';
+import 'package:booked_webapp_v1/routing/route_names.dart';
+import 'package:booked_webapp_v1/widgets/navigation_drawer/drawer_items.dart';
+
+class NavigationDrawerHome extends StatelessWidget {
+  const NavigationDrawerHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 16)]),
+      child: Column(
+        children: const <Widget>[
+          NavigationDrawerHeaderHome(),
+          //Bonus: Combine the ui for this widget with the navbaritem and make it responsive
+          DrawerItem('Profile', Icons.person, ProfileRoute),
+          DrawerItem('Recommendation', Icons.star, RecommendationRoute),
+          DrawerItem('Forum', Icons.forum, ForumRoute),
+          DrawerItem('About', Icons.help, AboutRoute),
+        ],
+      ),
+    );
+  }
+}
---
+++ bookedwebapp/booked_webapp_v1/lib/widgets/navigation_drawer/navDrawer_home/navDrawer_header_home.dart
@@ -0,0 +1,42 @@
+import 'package:booked_webapp_v1/locator.dart';
+import 'package:booked_webapp_v1/routing/route_names.dart';
+import 'package:booked_webapp_v1/services/navigation_service.dart';
+import 'package:flutter/material.dart';
+import 'package:booked_webapp_v1/constants/app_colors.dart';
+
+class NavigationDrawerHeaderHome extends StatelessWidget {
+  const NavigationDrawerHeaderHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // dont ever use a service directly in the ui to change any kind of state
+        // services should only be used from a viewmodel
+        locator<NavigationService>().navigateTo(RegisterRoute);
+      },
+      child: Container(
+        height: 150,
+        color: primaryColor,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const <Widget>[
+            Text(
+              'DISCUSS WITH BOOKWORMS',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'TAP TO GO TO FORUMS',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
