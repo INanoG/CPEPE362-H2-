39f8407bf18a90910d8628c1286fc3f0306b66aa
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-05-01 20:07:17 +0800

    some changes

--- bookedwebapp/booked_webapp_v1/lib/views/layout_template/layout_template_home.dart
+++ bookedwebapp/booked_webapp_v1/lib/views/layout_template/layout_template_home.dart
@@ -1,3 +1,4 @@
+import 'package:booked_webapp_v1/widgets/navigation_bar/navigation_bar_main/navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:booked_webapp_v1/locator.dart';
@@ -7,7 +8,6 @@ import 'package:booked_webapp_v1/services/navigation_service.dart';
 import 'package:booked_webapp_v1/views/welcome_page/welcome_page_desktop.dart';
 import 'package:booked_webapp_v1/views/welcome_page/welcome_page_mobile.dart';
 import 'package:booked_webapp_v1/widgets/centered_view/centered_view.dart';
-import 'package:booked_webapp_v1/widgets/navigation_bar/navigation_bar.dart';
 import 'package:booked_webapp_v1/widgets/navigation_drawer/navigation_drawer.dart';
 
 class LayoutTemplateHome extends StatelessWidget {
@@ -18,7 +18,7 @@ class LayoutTemplateHome extends StatelessWidget {
     return ResponsiveBuilder(
       builder: (context, sizingInformation) => Scaffold(
         drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
-            ? const NavigationDrawer()
+            ? NavigationDrawer()
             : null,
         backgroundColor: Colors.white,
         body: Centered_View(
--- bookedwebapp/booked_webapp_v1/lib/widgets/navigation_bar/navigation_bar_main/navigation_bar.dart
+++ bookedwebapp/booked_webapp_v1/lib/widgets/navigation_bar/navigation_bar_main/navigation_bar.dart
@@ -1,7 +1,7 @@
+import 'package:booked_webapp_v1/widgets/navigation_bar/navigation_bar_main/navigation_bar_tablet_desktop.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:booked_webapp_v1/widgets/navigation_bar/navigation_bar_main/navbar_mobile.dart';
-import 'package:booked_webapp_v1/widgets/navigation_bar/navigation_bar_main/navigation_bar_tablet_desktop.dart';
 
 class Navigation_Bar extends StatelessWidget {
   const Navigation_Bar({Key? key}) : super(key: key);
