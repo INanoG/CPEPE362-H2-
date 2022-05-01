c0b28a55a11024c7bd3bf7b96c188cbc44deea4e
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-05-01 15:13:53 +0800

    added ui for login page

--- bookedwebapp/booked_webapp_v1/lib/views/login/login_view.dart
+++ bookedwebapp/booked_webapp_v1/lib/views/login/login_view.dart
@@ -5,8 +5,131 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color.fromARGB(255, 243, 213, 255),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 480,
+                width: 395,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 224, 253, 203),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text(
+                      'Welcome Back',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        color: Color.fromARGB(255, 36, 56, 21),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Let us get you started',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 36, 56, 21),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const SizedBox(
+                      width: 250,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email Address',
+                          suffixIcon: Icon(Icons.mail, size: 17),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 250,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          suffixIcon: Icon(Icons.enhanced_encryption, size: 17),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 40, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Text(
+                            'Forget Password',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 131, 143, 139),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 250,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          gradient: const LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            colors: [
+                              Color.fromARGB(255, 78, 189, 135),
+                              Color.fromARGB(255, 86, 214, 152),
+                              Color.fromARGB(255, 78, 189, 135),
+                            ],
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
